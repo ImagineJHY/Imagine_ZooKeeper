2b0af1b220af27c651fef3aa68fa053650ee248a
Imagine <imaginejhy@163.com>
2023-12-13 16:54:21 +0800

    update

---
+++ include/Imagine_ZooKeeper/common_typename.h
@@ -0,0 +1,15 @@
+#ifndef IMAGINE_ZOOKEEPER_COMMON_TYPENAME_H
+#define IMAGINE_ZOOKEEPER_COMMON_TYPENAME_H
+
+#include "Imagine_Log/Imagine_Log.h"
+
+namespace Imagine_ZooKeeper
+{
+
+using Logger = ::Imagine_Tool::Imagine_Log::Logger;
+using SingletonLogger = ::Imagine_Tool::Imagine_Log::SingletonLogger;
+using NonSingletonLogger = ::Imagine_Tool::Imagine_Log::NonSingletonLogger;
+
+} // namespace Imagine_ZooKeeper
+
+#endif
